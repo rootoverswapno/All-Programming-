3eca100c8185761fa31010a023f73478b61a9b54
Swapnomoy Biswas <147144478+rootoverswapno@users.noreply.github.com>
2025-02-10 23:04:59 +0600

    Add files via upload

---
+++ Graph/Tree/Build_Max_Heap.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+void heapifyDown(vector<int>&arr,int i,int size)
+{
+    int largest=i;
+    int left=2*i+1;
+    int right=2*i+2;
+
+    if(left<size&&arr[left]>arr[largest])
+    {
+        largest=left;
+    }
+    if(right<size&&arr[right]>arr[largest])
+    {
+        largest=right;
+    }
+
+    if(i!=largest)
+    {
+        swap(arr[i],arr[largest]);
+        heapifyDown(arr,largest,size);
+    }
+}
+void buildMaxHeap(vector<int>&arr)
+{
+    int n=arr.size();
+    for(int i=(n/2)-1;i>=0;i--)
+    {
+        heapifyDown(arr,i,n);
+    }
+}
+//Function to print the heap
+void printheap(vector<int>&arr)
+{
+    for(int val:arr)
+    {
+        cout<<val<<" ";
+    }
+    cout<<endl;
+}
+int main()
+{
+    
+    vector<int>arr={10,20,15,30,40};
+    cout<<"Original Array : ";
+    printheap(arr);
+    buildMaxHeap(arr);
+     printheap(arr);
+
+
+return 0;
+}
---
+++ Graph/Tree/Max_Heap.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+void heapifyup(vector<int>&heap,int i)
+{
+    int parent=(i-1)/2;
+    if(i>0&&heap[i]>heap[parent]){
+        swap(heap[i],heap[parent]);
+        heapifyup(heap,parent);
+    }
+}
+void insert(vector<int>&heap,int val)
+{
+    heap.push_back(val);
+    heapifyup(heap,heap.size()-1);
+}
+int main()
+{
+      vector<int>heap;
+      
+    insert(heap, 50);
+    insert(heap, 30);
+    insert(heap, 40);
+    insert(heap, 10);
+    insert(heap, 20);
+    insert(heap, 15);
+     insert(heap, 60);
+
+    cout<<"MAx heap : ";
+    for(auto &i:heap)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+
+
+return 0;
+}
